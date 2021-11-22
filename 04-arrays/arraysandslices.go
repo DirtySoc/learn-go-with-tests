73c7d0703da6949281be03bac6bc537eb12dd212
Derek Willingham <derek.willingham@outlook.com>
2021-11-21 20:20:09 -0500

    complete arrays and slices

--- 04-arrays/arraysandslices.go
+++ 04-arrays/arraysandslices.go
@@ -1,9 +1,31 @@
 package arraysandslices
 
+// Sum returns the sum of all numbers in a slice
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll returns the sums of all slices
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+// SumAllTails returns the sums of all slices except the first element
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return
 }
